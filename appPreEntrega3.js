557d7b7ea52116071a9fdd3e80e404e2d0d75b55
Paulina Hoyos Burgos <pauly_hoyos48@hotmail.com>
2023-12-18 22:56:24 -0500

    JSComision49795_PreEntrega3_Hoyos

--- appPreEntrega3.js
+++ appPreEntrega3.js
@@ -1,3 +1,20 @@
+/*
+Comisión 49795 JavaScript
+
+Pre-Entrega 3
+
+Maria Paulina Hoyos Burgos
+*/
+
+/*
+Consigna:
+Una empresa de transporte llamada APP SERVICIOS necesita crear una aplicación para ir capturando los servicios requeridos por los usuarios, allí presentar un listado de opciones de rutas
+
+Una vez el usuario define: Nombre empresa, Nombre de contacto, Número de contacto, número de pasajeros, y seleccionar la ruta de una lista desplegable, se genera un reporte confirmando el agendamiento de su transporte
+
+MEJORA FUTURA: Se solicitará fecha y hora y se organizará el archivo según fecha y hora del servicios.
+*/
+
 ///Captura elementos del DOM
 let servicios = [];
 let valorServiciosCliente = [];
--- rutas.js
+++ rutas.js
@@ -12,8 +12,6 @@ Una empresa de transporte llamada APP SERVICIOS necesita crear una aplicación p
 
 Una vez el usuario define: Nombre empresa, Nombre de contacto, Número de contacto, número de pasajeros, y seleccionar la ruta de una lista desplegable, se genera un reporte confirmando el agendamiento de su transporte
 
-Se va generando para la empresa de transporte un archivo compilado con los servicios que van seleccionando para llevar control interno, en el cual se totaliza el numero de servicios, el total en dinero 
-
 MEJORA FUTURA: Se solicitará fecha y hora y se organizará el archivo según fecha y hora del servicios.
 */
 
